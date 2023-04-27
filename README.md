985ada536a57100a39d340b901cc4e39402bb20e
Gameiro <alexandrefgame@gmail.com>
2023-04-27 08:50:34 +0100

    Main Beginning

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # CC_Tweaked-Lib
 Library with all CC:Tweaked Lua implementions
+
+This is a repo with all Lua implementation that I made for CC:Tweaked.
+In the Librarys directory there's all the librarys made to day.
+
+# Main branch
+
+    PULL REQUESTS
+
+    To ask for a pull request to the main branch firstly delete any code and/or comments made in main.lua
+    This process needs to be done with the intent of giving anyone that uses this repo a blank canvas to start coding, 
+    instead of a full page of unreadable code
+
+# Branching tree
+
+    PULL REQUESTS
+
+    Contrary to the main branch, for pull requests you can leave the main.lua with any code considered essential to test the library you are working on.
+    Every branch needs to be named after the library it's being worked on.
---
+++ main.lua
@@ -0,0 +1,20 @@
+--[#]                                           [#]--
+ --     ███▄ ▄███▓ ▄▄▄       ██▓ ███▄    █        --
+ --     ▓██▒▀█▀ ██▒▒████▄    ▓██▒ ██ ▀█   █       --
+ --     ▓██    ▓██░▒██  ▀█▄  ▒██▒▓██  ▀█ ██▒      --
+ --     ▒██    ▒██ ░██▄▄▄▄██ ░██░▓██▒  ▐▌██▒      --
+ --     ▒██▒   ░██▒ ▓█   ▓██▒░██░▒██░   ▓██░      --
+ --     ░ ▒░   ░  ░ ▒▒   ▓▒█░░▓  ░ ▒░   ▒ ▒       --
+ --     ░  ░      ░  ▒   ▒▒ ░ ▒ ░░ ░░   ░ ▒░      --
+ --     ░      ░     ░   ▒    ▒ ░   ░   ░ ░       --
+ --            ░         ░  ░ ░           ░       --
+--[#]                                           [#]--
+
+-- This is the code testing ground. Here will be the place to put the code to test functions from API's and Libraries.
+-- To get access to this code in game, use pastebin, create an html reference (and use wget), etc, or access your
+-- CC:Tweaked/ComputerCraft computer folder.
+-- If you have no ideia how to access said folder, go to your minecraft directory and follow these simple steps:
+-- <minecraft directory> -> saves -> computerCraft -> computers -> <computer id> -> <put file wherever you want>
+
+
+--[[ Put your code after this ]]
