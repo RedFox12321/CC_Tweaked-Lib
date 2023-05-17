58b6f59c91417e3b8ce97570d5876457e7e183fd
Gameiro <alexandrefgame@gmail.com>
2023-05-17 20:44:07 +0100

    Added player detector

--- Libraries/Info.lua
+++ Libraries/Info.lua
@@ -18,29 +18,24 @@
 
 
 --####################--
+-- Required Dependencies:
+require "Peripherals"
 
+--####################--
 
--- [[ Refined Storage - rsBridge ]] --
+-- [[ Refined Storage - rsBridge]] --
 
+--#0
 function getRSItemStorageMaxCap(rsBridge)
-    if rsBridge == "undefined" then
-        print("Undefined peripheral.")
-       return
-    end
-    if peripheral.getType(rsBridge) ~= "rsBridge" then
-        print("Wrong peripheral type.")
+    if not isPeripheralType(rsBridge, "rsBridge") then
         return
     end
     return rsBridge.getMaxItemDiskStorage()
 end
 
+--#1
 function getRSItemStorage(rsBridge)
-    if rsBridge == "undefined" then
-        print("Undefined peripheral.")
-       return
-    end
-    if peripheral.getType(rsBridge) ~= "rsBridge" then
-        print("Wrong peripheral type.")
+    if not isPeripheralType(rsBridge, "rsBridge") then
         return
     end
     local count
@@ -54,25 +49,17 @@ function getRSItemStorage(rsBridge)
     return count
 end
 
+--#2
 function getRSFluidStorageMaxCap(rsBridge)
-    if rsBridge == "undefined" then
-        print("Undefined peripheral.")
-       return
-    end
-    if peripheral.getType(rsBridge) ~= "rsBridge" then
-        print("Wrong peripheral type.")
+    if not isPeripheralType(rsBridge, "rsBridge") then
         return
     end
-    print(rsBridge.getMaxFluidDiskStorage())
+    return rsBridge.getMaxFluidDiskStorage()
 end
 
+--#3
 function getRSFluidStorage(rsBridge)
-    if rsBridge == "undefined" then
-        print("Undefined peripheral.")
-       return
-    end
-    if peripheral.getType(rsBridge) ~= "rsBridge" then
-        print("Wrong peripheral type.")
+    if not isPeripheralType(rsBridge, "rsBridge") then
         return
     end
     local count
@@ -84,4 +71,130 @@ function getRSFluidStorage(rsBridge)
         end
     end
     return count
+end
+
+-- [[ Applied Energistics 2 - meBridge]] --
+
+--#4
+function getAEItemStorageMaxCap(meBridge)
+    if not isPeripheralType(meBridge, "meBridge") then
+        return
+    end
+    return meBridge.getTotalItemStorage()
+end
+
+--#5
+function getAEItemStorage(meBridge)
+    if not isPeripheralType(meBridge, "meBridge") then
+        return
+    end
+    return meBridge.getUsedItemStorage()
+end
+
+--#6
+function getAEFluidStorageMaxCap(meBridge)
+    if not isPeripheralType(meBridge, "meBridge") then
+        return
+    end
+    return meBridge.getTotalFluidStorage()
+end
+
+--#7
+function getAEFluidStorage(meBridge)
+    if not isPeripheralType(meBridge, "meBridge") then
+        return
+    end
+    return meBridge.getUsedFluidStorage()
+end
+
+-- [[ Player Detector - playerDetector ]] --
+
+function getOnlinePlayers(playerDetector)
+    if not isPeripheralType(playerDetector, "playerDetector") then
+        return
+    end
+    return playerDetector.getOnlinePlayers()
+end
+
+function getPlayersInRange(playerDetector, range)
+    if not isPeripheralType(playerDetector, "playerDetector") then
+        return
+    end
+    return playerDetector.getPlayersInRange(range)
+end
+
+function getPlayersInCoords(playerDetector, Xpos1, Ypos1, Zpos1, Xpos2, Ypos2, Zpos2)
+    if not isPeripheralType(playerDetector, "playerDetector") then
+        return
+    end
+    local pos1
+    local pos2
+    if type(Xpos1) == "table" and type(Ypos1) == "table" then
+        pos1 = Xpos1
+        pos2 = Ypos1
+    else
+        pos1 = {Xpos1, Ypos1, Zpos1}
+        pos2 = {Xpos2, Ypos2, Zpos2}
+    end
+    return playerDetector.getPlayersInCoords(pos1, pos2)
+end
+
+function getPlayersInCubic(playerDetector, Xsize, Ysize, Zsize, byRadius)
+    if not isPeripheralType(playerDetector, "playerDetector") then
+        return
+    end
+    byRadius = byRadius or false
+    if byRadius then
+        Xsize = Xsize*2; Ysize = Ysize*2; Zsize = Zsize*2
+    end
+    return playerDetector.getPlayersInCubic(Xsize, Ysize, Zsize)
+end
+
+function isPlayerInRange(playerDetector, username, range)
+    if not isPeripheralType(playerDetector, "playerDetector") then
+        return
+    end
+    if type(username) ~= "string" and type(username) ~= "table" then
+        return
+    end
+    local len
+    if type(username) == "string" then
+        return playerDetector.isPlayerInRange(range, username)
+    else
+        for i = 1, username.lenght(), 1 do
+            if playerDetector.isPlayerInRange(range, username[i]) then
+                return 1
+            end
+        end
+        return 0
+    end
+end
+
+function isPlayerInCoords(playerDetector, username, Xpos1, Ypos1, Zpos1, Xpos2, Ypos2, Zpos2)
+    if not isPeripheralType(playerDetector, "playerDetector") then
+        return
+    end
+    if type(username) ~= "string" and type(username) ~= "table" then
+        return
+    end
+    if type(Xpos1) == "table" and type(Ypos1) == "table" then
+        pos1 = Xpos1
+        pos2 = Ypos1
+    else
+        local pos1 = {Xpos1, Ypos1, Zpos1}
+        local pos2 = {Xpos2, Ypos2, Zpos2}
+    end
+    if type(username) == "string" then
+        return playerDetector.isPlayerInCoords(pos1, pos2, username)
+    else
+        for i = 1, username.lenght(), 1 do
+            if type(username[i]) ~= "string" then
+                return
+            end
+            if playerDetector.isPlayerInCoords(pos1, pos2, username[i]) then
+                return 1
+            end
+        end
+        return 0
+    end
 end
